868c8d79b0094717b52a03f46d7a8e8a8db483e4
FinesseKim <Brayokimeli@gmail.com>
2025-04-27 07:03:54 +0300

    Update api to render backend api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: 'https://health-info-system-fv7s.onrender.com/api', // Updated baseURL
   headers: {
     'Content-Type': 'application/json'
   }
